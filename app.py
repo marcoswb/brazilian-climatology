a0b9a1a987848ca1ab861db235e704a4e9ed8af1
marcoswb <marcos_wb@outlook.com>
2023-05-27 13:05:11 -0300

    create route to list stations

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 
 from serve_data.controllers.Root import Root
+from serve_data.controllers.Station import Station
 from serve_data.controllers.HistoryStation import HistoryStation
 from serve_data.controllers.HistoryState import HistoryState
 from serve_data.controllers.HistoryAverageStation import HistoryAverageStation
@@ -12,11 +13,17 @@ app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
+
+# gerais
 api.add_resource(Root, '/')
+api.add_resource(Station, '/station')
+
+# dados históricos
 api.add_resource(HistoryStation, '/history/station')
 api.add_resource(HistoryState, '/history/state')
 api.add_resource(HistoryAverageStation, '/history/average/station')
 api.add_resource(HistoryAverageState, '/history/average/state')
+
 
 if __name__ == '__main__':
     app.run()
--- serve_data/controllers/Station.py
+++ serve_data/controllers/Station.py
@@ -1,9 +1,42 @@
 from flask_restful import Resource
-from flask import Response
+from flask import jsonify, request
+
+from database.Postgre import Station as StationDatabase
+from utils.functions import *
 
 
-class Root(Resource):
+class Station(Resource):
 
     @staticmethod
     def get():
-        return Response(status=200, mimetype='application/json')
+        args = request.args
+        filter_state = args.get('state')
+
+        if filter_state is not None:
+            if len(filter_state) != 2:
+                response = jsonify({'message': f"Preencha com um valor válido o argumento 'state'"})
+                response.status_code = 400
+                return response
+
+            states = StationDatabase.select(StationDatabase.state)
+            list_states = [str(state_obj.state).upper() for state_obj in states]
+
+            if filter_state not in list_states:
+                response = jsonify({'message': f"Valor do argumento 'state' não consta no banco de dados."})
+                response.status_code = 400
+                return response
+
+        if filter_state is not None:
+            database_result = StationDatabase.select().where(StationDatabase.state == str(filter_state).upper()).order_by(StationDatabase.state, StationDatabase.name_station).dicts()
+        else:
+            database_result = StationDatabase.select().order_by(StationDatabase.state, StationDatabase.name_station).dicts()
+
+        response = {}
+        for line in database_result:
+            state = line.get('state')
+            if state not in response.keys():
+                response[state] = []
+
+            response[state].append(line)
+
+        return response
